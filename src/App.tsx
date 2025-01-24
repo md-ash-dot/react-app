452b5df28fc103b636ded4bed43b04da1917a580
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2025-01-24 21:01:00 +0000

    add Alert above button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
   return (
     <div>
+      <Alert>My alert</Alert>
       <Button onClick={() => console.log("Clicked")}>My Button</Button>
     </div>
   );
