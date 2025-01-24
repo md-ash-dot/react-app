ffc43b281b2f05a2c33824bf56fac5d2b840c519
MD ASH <129009358+md-ash-dot@users.noreply.github.com>
2025-01-24 19:19:54 +0000

    add sstyling to list items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,5 @@
 function ListGroup() {
   let items = ["New York", "San  Francisco", "Tokyo", "London", "Paris"];
-  items = [];
 
   return (
     <>
@@ -8,7 +7,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>{item}</li>
         ))}
       </ul>
     </>
